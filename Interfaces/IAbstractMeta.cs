72f34468852397e77bd552c5ace83e10f97665e1
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2021-07-08 14:58:23 -0700

    Autocommit
    Autocommit

--- Interfaces/IAbstractMeta.cs
+++ Interfaces/IAbstractMeta.cs
@@ -3,7 +3,6 @@
     /// <summary>
     /// Shared interface allowing an object to pass through the Meta Serialization system
     /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "<Pending>")]
     public interface IAbstractMeta : IMetaSerializable
     {
     }
