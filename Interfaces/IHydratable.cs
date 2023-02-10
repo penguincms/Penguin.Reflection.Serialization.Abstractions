454a30745ad93561fb61506b3314ce022b91964c
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-10 13:30:55 -0700

    Autocommit
    Autocommit

--- Interfaces/IHydratable.cs
+++ Interfaces/IHydratable.cs
@@ -5,6 +5,7 @@ namespace Penguin.Reflection.Serialization.Abstractions.Interfaces
     public interface IHydratable
     {
         int I { get; set; }
+
         bool IsHydrated { get; set; }
 
         void Hydrate(IDictionary<int, IHydratable> meta);
